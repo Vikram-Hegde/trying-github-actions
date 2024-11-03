47aed8fdd60a81eafd6cfad8d596f19d6e79df5b
Vikram <vikramhegde1440@gmail.com>
2024-11-03 17:45:19 +0530

    :bug: silly bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/trying-github-actions/'
+  base: '/trying-github-actions/',
   plugins: [react()],
 })
